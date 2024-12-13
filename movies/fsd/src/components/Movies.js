3e74bb0a2c0c324b95c24ddbd0dea9b775a2a932
Aaa-ananya <146376687+Aaa-ananya@users.noreply.github.com>
2024-12-13 12:57:02 +0530

    commit

---
+++ movies/fsd/src/components/Movies.js
@@ -0,0 +1,67 @@
+const Movies=[
+    {
+id:1,
+image:"https://imgs.search.brave.com/ieCtOoPnEXKTr46DOzkqJ3J4l9NWagRHXHI3j6f86Bc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzI5L2Nj/L2U2LzI5Y2NlNjg4/M2FlYTllZDFjZjg4/OTk1NTRjZTExMTZh/LmpwZw",
+title:"Mother India",
+language:"Hindi",
+cost:200,
+    },
+    {
+    id:2,
+image:"https://imgs.search.brave.com/pbQf6VtPtBkgBGPSu1xsSynwzs0wJlTUHmrE8aHN8yQ/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3F1YXJlc3Bh/Y2UtY2RuLmNvbS9j/b250ZW50L3YxLzVh/NjA4ZTRlMDE3ZGIy/ZThkZWRiMjQyYi8x/NTE3Mzg0ODYwMDAz/LTJMNE5OMU4zNEY2/WU1DMlVFSUVULzYr/LStLQVRJK1BBVEFO/Ry5KUEc",
+title:"Kati Patang",
+language:"Hindi",
+cost:200,
+    },
+    {
+        id:3,
+    image:"https://imgs.search.brave.com/WYTWVawaCF7Wow0vSZNak4Awaa2kHnK9HISXyFMCJhY/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzI1LzE4/LzZkLzI1MTg2ZGJh/MTdmZmE5ZTA3NWYx/ZTVkZGI4M2NkMWZh/LmpwZw",
+    title:"Noor Jahan",
+    language:"Hindi",
+    cost:200,
+        },
+        {
+            id:4,
+        image:"https://imgs.search.brave.com/HCoV2HhXJnVMEtuZVBXHMlO8bSLJvvYPAv4qm2zuuIg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzViLzgy/L2IxLzViODJiMTg5/NDlkZTMzZGVkYTc1/MjRkNzY1NDA0Njkw/LmpwZw",
+        title:"Anarkali",
+        language:"Hindi",
+        cost:200,
+            },
+            {
+                id:5,
+            image:"https://imgs.search.brave.com/4TqhFyEVzprh-PP6qUom4TBE18Z8gkQg20STj2E_YvU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3F1YXJlc3Bh/Y2UtY2RuLmNvbS9j/b250ZW50L3YxLzVh/NjA4ZTRlMDE3ZGIy/ZThkZWRiMjQyYi8x/NTE3Mzg0NzY2MTE0/LU1VRThBMzBQQUdF/QUpJSTVNVU1DLzMr/LStERUVXQVIuSlBH",
+            title:"Deewar",
+            language:"Hindi",
+            cost:200,
+                },
+                {
+                    id:6,
+                image:"https://imgs.search.brave.com/EDqm7LEV1vuzsdMEgPovQQYe1G9sO6GKoH3_ULGsTDc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzF2UC1jTndoTUwu/anBn",
+                title:"Mera Naam Joker",
+                language:"Hindi",
+                cost:200,
+                    },
+                    {
+                        id:7,
+                    image:"https://imgs.search.brave.com/AhjRHW3Efiq00WC3wDC3XXJAI35rlkJn0VhXIujYRQ8/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc3F1YXJlc3Bh/Y2UtY2RuLmNvbS9j/b250ZW50L3YxLzVh/NjA4ZTRlMDE3ZGIy/ZThkZWRiMjQyYi8x/NTE3Mzg0Nzk5OTAw/LUFTREc0Q0lXTVhY/VlBCQkhSVUlULzEr/LStTSE9MQVkuSlBH",
+                    title:"Sholay",
+                    language:"Hindi",
+                    cost:200,
+                        },
+                        {
+                            id:8,
+                        image:"https://imgs.search.brave.com/xC1aAboQQu73Cwg-m2VecM9fk7ANkTxzhh8PCl7fOOY/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NzFBVVFoRUZWSkwu/anBn",
+                        title:"Mughal-e-azam",
+                        language:"Hindi",
+                        cost:200,
+                            },
+                            {
+                                id:9,
+                            image:"https://imgs.search.brave.com/UY-Zhb6lJ6RqWwTfIdigSaLCoSjPI5RwOoT170AH33o/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9wb3N0/ZXJhbGx5LmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAxOS8w/Ni8wSTdBOTM2Ny1l/MTU1OTU2NzQzODk4/OC0zMDB4NDUwLmpw/Zw",
+                            title:"Umrao Jaan",
+                            language:"Hindi",
+                            cost:200,
+                                },
+                                
+]
+export default Movies;
